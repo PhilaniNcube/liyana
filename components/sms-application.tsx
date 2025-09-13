53b8777a9c98156e18ef9e6b64d7c16556075f66
Philani Ncube <ncbphi001@gmail.com>
2025-09-13 20:25:43 +0200

    style: Update layout of SmsApplication component for improved responsiveness

--- components/sms-application.tsx
+++ components/sms-application.tsx
@@ -112,7 +112,7 @@ export default function SmsApplication({
   };
 
   return (
-    <div className="space-y-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
       {/* Send SMS Card */}
       <Card>
         <CardHeader>
