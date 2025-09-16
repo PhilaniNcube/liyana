880e6c1af3f3a11fb4047d979f11df6a4f1bec40
Philani Ncube <ncbphi001@gmail.com>
2025-09-16 15:57:02 +0200

    feat: Adjust layout for funeral premium calculator page to improve responsiveness

--- app/(public)/calculate-premium/page.tsx
+++ app/(public)/calculate-premium/page.tsx
@@ -4,7 +4,7 @@ import FuneralPremiumCalculatorDialog from "../insurance/funeral/_components/fun
 const page = () => {
   return (
     <div>
-      <div className="bg-gray-100 py-12 min-h-screen">
+      <div className="bg-gray-100 py-12 h-[calc(100vh-6rem)] flex flex-col justify-center">
         <div className="space-y-4">
           <p className="text-muted-foreground max-w-2xl mx-auto">
             Get comprehensive funeral cover for you and your family. Calculate
