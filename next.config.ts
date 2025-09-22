3bf2d6517099334403e85891ff0fa5c508284ca9
Philani Ncube <ncbphi001@gmail.com>
2025-09-22 15:59:15 +0200

    feat: Add image quality settings to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    qualities: [25, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
+  },
   experimental: {
     browserDebugInfoInTerminal: true,
     ppr: true,
